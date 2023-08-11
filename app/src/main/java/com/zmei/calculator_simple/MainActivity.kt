0a478dd18cbd7f2d3853c7a8e30774095c65bbe8
Arhizmei <arhizmei@gmail.com>
2023-08-11 15:20:56 +0300

    init

---
+++ app/src/main/java/com/zmei/calculator_simple/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zmei.calculator_simple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
